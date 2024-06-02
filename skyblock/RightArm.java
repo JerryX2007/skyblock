1bc1f88da5cf7e554bf99a3f4baf61ff918cc326
TittoPlayer <161748440+TittoPlayer@users.noreply.github.com>
2024-06-02 01:39:44 -0400

    punching animation

--- skyblock/RightArm.java
+++ skyblock/RightArm.java
@@ -10,11 +10,18 @@ public class RightArm extends BodyPart
 {
     private GreenfootImage img;
     private Steve steve;
+    private int actNum;
+    private boolean isPunching;
+    private GreenfootImage img1;
     public RightArm(Steve steve){
         img = new GreenfootImage("steve/arm_right.png");
         img.scale(16,96);
         setImage(img);
         this.steve = steve;
+        isPunching = false;
+        img1 = new GreenfootImage("steve/side_arm.png");
+        img1.scale(16,96);
+        actNum = 0;
     }
     /**
      * Act - do whatever the LeftArm wants to do. This method is called whenever
@@ -25,4 +32,5 @@ public class RightArm extends BodyPart
         // Add your action code here.
         setLocation(steve.getX(), steve.getY()-32);
     }
+
 }
