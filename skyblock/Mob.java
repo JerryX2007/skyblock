660590f477ca02813c286e58954d77ec7fad0852
TheDankDork <150256710+TheDankDork@users.noreply.github.com>
2024-06-11 22:34:29 -0400

    Worked on some movement AI for mobs
    Around 30% complete.  Still need to automate movement and check for bugs.

--- skyblock/Mob.java
+++ skyblock/Mob.java
@@ -1,4 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.Random;
 
 /**
  * All mobs are born with its own damage, speed, and health (passive mobs have 0 damage)
@@ -20,11 +21,17 @@ public abstract class Mob extends SuperSmoothMover{
     protected double speed;
     protected int health;
     
+    protected double yVelocity;
+    protected double acceleration = 0.15;
+    
     protected int minSpawnLight;
     protected int maxSpawnLight;
 
     protected boolean isFleeing;
     SimpleTimer fleeTimer;
+    
+    SimpleTimer wanderTimer;
+    Random random;
     
     protected GreenfootImage defaultImg;
     protected GreenfootImage movingImg;
@@ -99,7 +106,13 @@ public abstract class Mob extends SuperSmoothMover{
      * If there is nothing to do, wander aimlessly around
      */
     protected void wander(){
-        
+        if(wanderTimer.millisElapsed() > 3000){
+            int direction = random.nextInt(2);
+            
+            if(direction == 0){
+                
+            }
+        }
     }
     
     /**
@@ -130,5 +143,164 @@ public abstract class Mob extends SuperSmoothMover{
             }
         }
         return false;
+    }
+    
+    /**
+     * Checks if the mob in on solid ground
+     * Gets the block directly under the mob.  If there exists a block and it isn't an air block, return true.
+     */
+    protected boolean onGround() {
+        Block under = (Block) getOneObjectAtOffset(0, getImage().getHeight()/2, Block.class);
+        if(under != null) {
+            if(under instanceof Air) {
+                return false;
+            }
+            else {
+                return true;
+            }
+        }        
+        return false;
+    }
+
+    /**
+     * Checks if the mob's head is clear and determines if it can jump
+     * Gets the block directly above the mob.  If there exists a block that isn't an air block, return false.  Otherwise return true.
+     */
+    protected boolean headClear(){
+        Block above = (Block) getOneObjectAtOffset(0, -(getImage().getHeight()/2+4), Block.class);
+        if(above != null) {
+            if(above instanceof Air) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }        
+        return true;        
+    }
+
+    /**
+     * Checks if the mob can continue moving right
+     * Uses 4 collision points to the right of the mob to detect any non-air blocks
+     * If either of the 4 collision points detect a non-air block, immediately return false
+     */
+    protected boolean rightClear(){ 
+        Block right = (Block) getOneObjectAtOffset(getImage().getWidth()/2 + 5, getImage().getHeight()/4, Block.class);
+        if(right != null) {
+            if(!(right instanceof Air)){
+                return false;
+            }
+        }
+        right = (Block) getOneObjectAtOffset(getImage().getWidth()/2 + 5, (getImage().getHeight()/4) * -1, Block.class);
+        if(right != null) {
+            if(!(right instanceof Air)){
+                return false;
+            }
+        }
+        right = (Block) getOneObjectAtOffset(getImage().getWidth()/2 + 5, getImage().getHeight()/2 - 5, Block.class);
+        if(right != null) {
+            if(!(right instanceof Air)){
+                return false;
+            }
+        }
+        right = (Block) getOneObjectAtOffset(getImage().getWidth()/2 + 5, (getImage().getHeight()/2) * -1, Block.class);
+        if(right != null) {
+            if(!(right instanceof Air)){
+                return false;
+            }
+        }
+        return true;
+    }    
+    
+    /**
+     * Checks if the mob can continue moving left
+     * Uses 4 collision points to the left of the mob to detect any non-air blocks
+     * If either of the 4 collision points detect a non-air block, immediately return false
+     */
+    protected boolean leftClear(){
+        Block left = (Block) getOneObjectAtOffset((getImage().getWidth()/2 + 5) * -1, getImage().getHeight()/4, Block.class);
+        if(left != null) {
+            if(!(left instanceof Air)){
+                return false;
+            }
+        }
+        left = (Block) getOneObjectAtOffset((getImage().getWidth()/2 + 5) * -1, (getImage().getHeight()/4) * -1, Block.class);
+        if(left != null) {
+            if(!(left instanceof Air)){
+                return false;
+            }
+        }
+        left = (Block) getOneObjectAtOffset((getImage().getWidth()/2 + 5) * -1, getImage().getHeight()/2 - 5, Block.class);
+        if(left != null) {
+            if(!(left instanceof Air)){
+                return false;
+            }
+        }
+        left = (Block) getOneObjectAtOffset((getImage().getWidth()/2 + 5) * -1, (getImage().getHeight()/2) * -1, Block.class);
+        if(left != null) {
+            if(!(left instanceof Air)){
+                return false;
+            }
+        }
+
+        return true;
+    }
+    
+    /**
+     * Moves left by a pixel for each move speed
+     * If its path is blocked by a single block, will jump over it.  Otherwise break early.
+     */
+    protected void moveLeft(){
+        for(int i = 0; i < speed; i++){
+            setLocation(getX() - 1, getY());
+            if(!leftClear()){
+                if(headClear()){
+                    jump();
+                }
+            }
+        }
+    }
+        
+    /**
+     * Moves right by a pixel for each move speed
+     * If its path is blocked by a single block, will jump over it.  Otherwise break early.
+     */
+    protected void moveRight(){
+        for(int i = 0; i < speed; i++){
+            setLocation(getX() + 1, getY());
+            if(!rightClear()){
+                if(headClear()){
+                    jump();
+                }
+            }
+        }
+    }
+    
+    /**
+     * If it isn't on the ground, start falling
+     */
+    protected void checkFalling() {
+        if(onGround()) {
+            yVelocity = 0;
+        }
+        else {
+            fall();
+        }
+    }
+
+    /**
+     * Accelerate downwards to fall
+     */
+    protected void fall() {
+        setLocation(getX(), getY() + yVelocity);
+        yVelocity = yVelocity + acceleration;
+    }
+
+    /**
+     * Gains a small amount of momentum upwards to jump
+     */
+    protected void jump() {
+        yVelocity -= 4.9;
+        setLocation(getX(), getY() + yVelocity);
     }
 }
