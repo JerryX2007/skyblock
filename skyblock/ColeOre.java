f234e5e9d1c4c7f9a9e36f4f9b3265285cceffa9
DylanDinesh <350444048@gapps.yrdsb.ca>
2024-06-15 20:51:04 -0400

    commit

---
+++ skyblock/ColeOre.java
@@ -0,0 +1,39 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class ColeOre here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ColeOre extends Block
+{
+    private GreenfootImage img;
+    private GreenfootImage img2;
+
+    public ColeOre(){
+        super(Color.GRAY,5, "cole_ore");
+        img = new GreenfootImage("block/cobblestone.png");
+        img.scale(64,64);
+        setImage(img);
+        img2 = addBorder(img, black);
+        isStone = true;
+        itemDrop = 1;
+    }
+
+    /**
+     * Act - do whatever the Grass wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+        super.act();
+        if(isSelected){
+            setImage(img2);
+        }
+        if(!isSelected){
+            setImage(img);
+        }
+    }
+}
